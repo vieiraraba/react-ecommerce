1baeeceb55556ca10a690411ec19a819743f1dca
Ricardo Vieira <vieiraraba@gmail.com>
2022-07-08 08:22:35 +0200

    feat(ShoppingCart):"making the function ShoppingCart"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
-import "./App.css";
-import { useState } from "react";
-import Product from "./components/Product/Product";
-import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
-
+import { useEffect, useState } from "react";
+import { Button } from "@mui/material";
 import { products } from "./data/products";
 
+import Product from "./components/Product/Product";
+import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+
+import "./App.css";
+
+const initialShoppingCart =
+  JSON.parse(localStorage.getItem("shoppingCart")) || [];
+
 function App() {
-  const [buyShoppingCart, setBuyShoppingCart] = useState([]);
+  const [buyShoppingCart, setBuyShoppingCart] = useState(initialShoppingCart);
+  const [state, setState] = useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  useEffect(() => {
+    localStorage.setItem("shoppingCart", JSON.stringify(buyShoppingCart));
+  }, [buyShoppingCart]);
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+    console.log("Algoo");
+  };
 
   return (
     <div className="main__app">
       <div className="main__products">
         <h1 className="main__title">Keeanu Reeves</h1>
+        <Button onClick={toggleDrawer("right", true)}>Open Menu</Button>
         <div className="products_list">
           {products.map((item, index) => {
             return (
@@ -25,12 +55,25 @@ function App() {
                 image={item.image}
                 cart={buyShoppingCart}
                 setCart={setBuyShoppingCart}
+                menuState={state}
+                setMenu={setState}
               />
             );
           })}
         </div>
       </div>
-      <ShoppingCart cart={buyShoppingCart} setCart={setBuyShoppingCart} />
+      <Drawer
+        className="test"
+        anchor="right"
+        open={state["right"]}
+        onClose={toggleDrawer("right", false)}
+      >
+        {/* {list(anchor)} */}
+        <h1>Shopping Cart</h1>
+        <Divider />
+
+        <ShoppingCart cart={buyShoppingCart} setCart={setBuyShoppingCart} />
+      </Drawer>
     </div>
   );
 }
--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,33 +1,53 @@
-import "./ShoppingCart.css";
+import { useState, useEffect } from "react";
 import Button from "@mui/material/Button";
 import CartItem from "../CartItem/CartItem";
 
+import "./ShoppingCart.css";
+
 const ShoppingCart = ({ cart, setCart }) => {
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    let totalPrice = 0;
+
+    cart.forEach((item) => {
+      totalPrice += item.cartPrice * item.amount;
+    });
+
+    setTotal(totalPrice);
+  }, [cart]);
+
   return (
     <div className="main__cart">
-      <h3 className="cart-title">Shopping Cart</h3>
       <ul className="cart__list">
-        {cart.map((item, index) => {
-          return (
-            <CartItem
-              key={index}
-              id={item.cartId}
-              title={item.cartTitle}
-              price={item.cartPrice}
-              image={item.cartImage}
-            />
-          );
-        })}
+        {cart.length === 0 ? (
+          <p>No items in the cart</p>
+        ) : (
+          <div>
+            {cart.map((item, index) => {
+              return (
+                <CartItem
+                  key={index}
+                  id={item.cartId}
+                  title={item.cartTitle}
+                  price={item.cartPrice}
+                  image={item.cartImage}
+                  quantity={item.amount}
+                  cart={cart}
+                  setCart={setCart}
+                />
+              );
+            })}
+          </div>
+        )}
       </ul>
       <div className="cart__bottom">
         <p className="cart__total">Total:</p>
-        <p className="cart__total_price">€0</p>
+        <p className="cart__total_price">€{total}</p>
       </div>
-      <Button
-        variant="outlined"
-        color="error"
-        size="large">
-          Checkout</Button>
+      <Button variant="outlined" color="error" size="large">
+        Checkout
+      </Button>
     </div>
   );
 };
