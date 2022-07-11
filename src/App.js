19b7c1660d550093ad0ea41cf2b7554804f73523
Ricardo Vieira <vieiraraba@gmail.com>
2022-07-11 17:29:34 +0200

    modify(Navbar):"css and jsx "

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import { useEffect, useState } from "react";
 
-import { products } from "./data/products";
-
-import Product from "./components/Product/Product";
-import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
 import Navbar from "./components/Navbar/Navbar";
 
 import "./App.css";
@@ -12,48 +8,9 @@ const initialShoppingCart =
   JSON.parse(localStorage.getItem("shoppingCart")) || [];
 
 function App() {
-  const [buyShoppingCart, setBuyShoppingCart] = useState(initialShoppingCart);
-  
-
-  useEffect(() => {
-    localStorage.setItem("shoppingCart", JSON.stringify(buyShoppingCart));
-  }, [buyShoppingCart]);
-
-
   return (
     <div className="main__app">
-      <div className="main__products">
-        <h1 className="main__title">Jean Claude Van Damme</h1>
-
-        <div className="products_list">
-          {products.map((item, index) => {
-            return (
-              <Product
-                key={index}
-                id={index}
-                title={item.title}
-                description={item.description}
-                valoration={item.valoration}
-                price={item.price}
-                image={item.image}
-                cart={buyShoppingCart}
-                setCart={setBuyShoppingCart}
-
-              />
-            );
-          })}
-        </div>
-      </div>
-      <div
-        className="test"
-        anchor="right"
-      >
-        {/* {list(anchor)} */}
-        <h1>Shopping Cart</h1>
-
-
-        <ShoppingCart cart={buyShoppingCart} setCart={setBuyShoppingCart} />
-      </div>
+      <Navbar />
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import { Popover, Button, Text } from "@nextui-org/react";
 
-function Navbar() {
+import "./Navbar.css";
+
+
+const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar__container">
+
+        <div className="navbar__container_logo">
+            <img src="" alt="Logo" />
+        </div>
+
+        <div className="navbar__container_links">
+        <span className="navbar__container_link
+        ">DEMO</span>
+        <span className="navbar__container_link
+        ">SHOP</span>
+        <span className="navbar__container_link
+        ">PRODUCT</span>
+        <span className="navbar__container_link
+        ">PORTFOLIO</span>
+        <span className="navbar__container_link
+        ">LOOKBOK</span>
+        <span className="navbar__container_link
+        ">BLOG</span>
+        </div>
+
+        <div className="navbar__container_icons">
+        <Popover>
+      <Popover.Trigger>
+        <Button auto flat>
+            <img src={require("../../assets/icons/Buy.svg")}alt="icon" />
+        </Button>
+      </Popover.Trigger>
+      <Popover.Content>
+        <Text css={{ p: "$10" }}>This is the content of the popover.</Text>
+      </Popover.Content>
+    </Popover>
+        </div>
+
+    </div>
   )
 }
 
