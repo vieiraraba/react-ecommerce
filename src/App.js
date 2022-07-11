545348aa28018c2e0efdca6c5c003752edcce298
Ricardo Vieira <vieiraraba@gmail.com>
2022-07-11 16:35:41 +0200

    feat(navbar):"creating navbar"

--- src/App.js
+++ src/App.js
@@ -1,48 +1,30 @@
 import { useEffect, useState } from "react";
-import { Button } from "@mui/material";
+
 import { products } from "./data/products";
 
 import Product from "./components/Product/Product";
 import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
+import Navbar from "./components/Navbar/Navbar";
 
 import "./App.css";
 
 const initialShoppingCart =
   JSON.parse(localStorage.getItem("shoppingCart")) || [];
 
-/* The above code is a React component that renders a list of products. */
 function App() {
   const [buyShoppingCart, setBuyShoppingCart] = useState(initialShoppingCart);
-  const [state, setState] = useState({
-    top: false,
-    left: false,
-    bottom: false,
-    right: false,
-  });
+  
 
   useEffect(() => {
     localStorage.setItem("shoppingCart", JSON.stringify(buyShoppingCart));
   }, [buyShoppingCart]);
 
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
-      return;
-    }
-
-    setState({ ...state, [anchor]: open });
-    console.log("Algoo");
-  };
 
   return (
     <div className="main__app">
       <div className="main__products">
-        <h1 className="main__title">Keeanu Reeves</h1>
-        <Button onClick={toggleDrawer("right", true)}>Open Menu</Button>
+        <h1 className="main__title">Jean Claude Van Damme</h1>
+
         <div className="products_list">
           {products.map((item, index) => {
             return (
@@ -56,25 +38,22 @@ function App() {
                 image={item.image}
                 cart={buyShoppingCart}
                 setCart={setBuyShoppingCart}
-                menuState={state}
-                setMenu={setState}
+
               />
             );
           })}
         </div>
       </div>
-      <Drawer
+      <div
         className="test"
         anchor="right"
-        open={state["right"]}
-        onClose={toggleDrawer("right", false)}
       >
         {/* {list(anchor)} */}
         <h1>Shopping Cart</h1>
-        <Divider />
+
 
         <ShoppingCart cart={buyShoppingCart} setCart={setBuyShoppingCart} />
-      </Drawer>
+      </div>
     </div>
   );
 }
